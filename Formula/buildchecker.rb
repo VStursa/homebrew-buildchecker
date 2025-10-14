54e73ce8369a77195103602e91531ba1a77fd495
Asistent Bohus <ios@firma.seznam.cz>
2025-10-14 16:04:43 +0200

    Update buildchecker to version v1.0.25 (universal binaries for ARM64 and x86_64)

--- Formula/buildchecker.rb
+++ Formula/buildchecker.rb
@@ -6,11 +6,11 @@ class Buildchecker < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/buildchecker/buildchecker-v1.0.24-arm64-apple-darwin.tar.gz"
-      sha256 "0811a00e1a604d7dbb87638eebdbb4c41c22a77ed6b1af580f1e0e28e2cb9840"
+      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/buildchecker/buildchecker-v1.0.25-arm64-apple-darwin.tar.gz"
+      sha256 "e60e520ef333a5db3ec77f4fd1ff5c2da2b2c11f685762379d98073ec2a31db6"
     else
-      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/buildchecker/buildchecker-v1.0.24-x86_64-apple-darwin.tar.gz"
-      sha256 "e8293df2198622733d961b9e3b1538c251a01bea9b6d500ca4bc77f2dbc8fe4f"
+      url "https://sbrowser.dev.dnsz.cz/vojtech.stursa/artefacts/buildchecker/buildchecker-v1.0.25-x86_64-apple-darwin.tar.gz"
+      sha256 "a9ae34573535254bab84c5da4c4ccb35b8bd21d1861d570cd68b8b2d4d632d2f"
     end
   end
 
@@ -33,6 +33,6 @@ class Buildchecker < Formula
     assert_match "Test", output
 
     # Test version
-    assert_match "v1.0.24", shell_output("#{bin}/buildchecker --version")
+    assert_match "v1.0.25", shell_output("#{bin}/buildchecker --version")
   end
 end
